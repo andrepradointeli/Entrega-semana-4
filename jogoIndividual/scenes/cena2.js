1a3b78946a956cae63081f3f36874466c7d3d201
andrepradointeli <159079743+andrepradointeli@users.noreply.github.com>
2024-02-26 13:23:56 -0300

    dsa

--- jogoIndividual/scenes/cena2.js
+++ jogoIndividual/scenes/cena2.js
@@ -2,11 +2,11 @@ class cena2 extends Phaser.Scene {
 
     // Construtor da cena
     constructor() {
-        super({
-            key: 'cena2',
-            
-        });
-    }
+        super({ key: 'cena2' });
+            this.tijolos;
+            this.base;
+            this.bola
+        };
 
     preload() {
 
@@ -16,6 +16,13 @@ class cena2 extends Phaser.Scene {
 
     create () {
         this.add.image(400, 300, 'fundo')
+
+        this.physics.world.setBoundsCollision(true, true, true, false);
+
+        this.bricks = this.physics.add.staticGroup({
+            
+            gridAlign: { width: 10, height: 6, cellWidth: 64, cellHeight: 32, x: 112, y: 100 }
+        });
       
     }
 
